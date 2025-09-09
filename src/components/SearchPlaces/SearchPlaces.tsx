7cd38ff9f2064d5c79712a731cee21f5bb4e5753
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2025-09-09 12:31:19 +0300

    Added svgr plugin for vite
    Made the Search section look ok on mobile

--- src/components/SearchPlaces/SearchPlaces.tsx
+++ src/components/SearchPlaces/SearchPlaces.tsx
@@ -1,4 +1,4 @@
-import SearchIcon from "../../images/icon-search.svg";
+import SearchIcon from "../../images/icon-search.svg?react";
 
 import Button from "../Button/Button";
 
@@ -8,9 +8,13 @@ const SearchPlaces = () => {
       <h1 className="font-bricolage-grotesque text-6xl text-center mb-[4.25rem] font-bold">
         How's the sky looking today?
       </h1>
-      <div className="bg-background-2 mb-[0.75rem]">
-        <img src={SearchIcon} alt="" />
-        <input type="text" placeholder="Search for a place..." />
+      <div className="bg-background-2 mb-[0.75rem] relative rounded-[0.625rem] ">
+        <SearchIcon className="absolute top-[1.125rem] left-[1.5rem] text-grayish-white" />
+        <input
+          type="text"
+          placeholder="Search for a place..."
+          className="w-full h-[3.625rem] pl-[3.875rem]"
+        />
       </div>
       <Button title="Search" />
     </div>
