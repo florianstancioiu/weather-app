0906487ffe6c3b05579a4248de5cf119c5ed5392
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2025-09-09 13:39:56 +0300

    Worked on the mobile HourlyForecast components

--- src/components/HourlyForecast/HourlyForecast.tsx
+++ src/components/HourlyForecast/HourlyForecast.tsx
@@ -1,5 +1,43 @@
+import DaysDropdown from "../DaysDropdown/DaysDropdown";
+import HourlyForecastItem from "../HourlyForecastItem/HourlyForecastItem";
+
+import IconRain from "../../images/icon-rain.webp";
+import IconDrizzle from "../../images/icon-drizzle.webp";
+import IconFog from "../../images/icon-fog.webp";
+import IconStorm from "../../images/icon-storm.webp";
+import IconOvercast from "../../images/icon-overcast.webp";
+import IconSunny from "../../images/icon-sunny.webp";
+import IconSnow from "../../images/icon-snow.webp";
+
 const HourlyForecast = () => {
-  return <aside></aside>;
+  return (
+    <aside className="mt-[2rem] py-[1.25rem] px-[1rem] bg-background-2 rounded-[0.625rem]">
+      <div className="flex justify-between items-center">
+        <h3 className="mb-[1.5rem]">Hourly forecast</h3>
+        <DaysDropdown />
+      </div>
+      <div className="max-h-[37rem] overflow-scroll grid gap-[1rem] grid-cols-1">
+        <HourlyForecastItem hour="3 PM" temperature="20°" image={IconRain} />
+        <HourlyForecastItem hour="4 PM" temperature="20°" image={IconDrizzle} />
+        <HourlyForecastItem hour="5 PM" temperature="20°" image={IconFog} />
+        <HourlyForecastItem hour="6 PM" temperature="20°" image={IconStorm} />
+        <HourlyForecastItem
+          hour="7 PM"
+          temperature="20°"
+          image={IconOvercast}
+        />
+        <HourlyForecastItem hour="8 PM" temperature="20°" image={IconSunny} />
+        <HourlyForecastItem hour="9 PM" temperature="20°" image={IconSnow} />
+        <HourlyForecastItem hour="10 PM" temperature="20°" image={IconRain} />
+        <HourlyForecastItem hour="11 PM" temperature="20°" image={IconRain} />
+        <HourlyForecastItem hour="12 PM" temperature="20°" image={IconRain} />
+        <HourlyForecastItem hour="1 AM" temperature="20°" image={IconRain} />
+        <HourlyForecastItem hour="2 AM" temperature="20°" image={IconRain} />
+        <HourlyForecastItem hour="3 AM" temperature="20°" image={IconRain} />
+        <HourlyForecastItem hour="4 AM" temperature="20°" image={IconRain} />
+      </div>
+    </aside>
+  );
 };
 
 export default HourlyForecast;
--- src/components/HourlyForecastItem/HourlyForecastItem.tsx
+++ src/components/HourlyForecastItem/HourlyForecastItem.tsx
@@ -1,5 +1,23 @@
-const HourlyForecastItem = () => {
-  return <div></div>;
+export type HourlyForecastItem = {
+  hour: string;
+  temperature: string;
+  image: string;
+};
+
+const HourlyForecastItem = ({
+  hour,
+  temperature,
+  image,
+}: HourlyForecastItem) => {
+  return (
+    <div className="bg-background-3 rounded-[5px] flex justify-between items-center h-[3.75rem] px-[1.125rem] border-[1px] border-light-blue">
+      <div className="flex items-center gap-[1rem]">
+        <img src={image} alt="" className="w-[40px]" />
+        <p>{hour}</p>
+      </div>
+      <p>{temperature}</p>
+    </div>
+  );
 };
 
 export default HourlyForecastItem;
