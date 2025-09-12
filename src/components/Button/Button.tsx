97f467cf412754f275024205e05d05c9590ec6c1
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2025-09-12 11:57:30 +0300

    Added onClick prop to Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,14 @@
 export type Button = {
   title: string;
+  onClick: () => void;
 };
 
-const Button = ({ title }: Button) => {
+const Button = ({ title, onClick }: Button) => {
   return (
-    <button className="bg-blue rounded-[0.625rem] w-full h-[3.5rem] cursor-pointer dsktp:w-auto dsktp:px-[1.5rem]">
+    <button
+      onClick={onClick}
+      className="bg-blue rounded-[0.625rem] w-full h-[3.5rem] cursor-pointer dsktp:w-auto dsktp:px-[1.5rem]"
+    >
       {title}
     </button>
   );
