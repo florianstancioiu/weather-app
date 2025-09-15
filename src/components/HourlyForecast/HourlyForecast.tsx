71535e3dd48469fcb6b9b73952d5d857e48b8649
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2025-09-15 17:18:15 +0300

    Worked on making the HourlyForecast functional

--- src/components/HourlyForecast/HourlyForecast.tsx
+++ src/components/HourlyForecast/HourlyForecast.tsx
@@ -1,67 +1,56 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import DaysDropdown from "../DaysDropdown/DaysDropdown";
 import HourlyForecastItem from "../HourlyForecastItem/HourlyForecastItem";
-import { type DayDropdownValue } from "../DaysDropdown/DaysDropdown";
+import { type DayDropdownValue, type Day } from "../DaysDropdown/DaysDropdown";
 
-import IconRain from "../../images/icon-rain.webp";
-import IconDrizzle from "../../images/icon-drizzle.webp";
-import IconFog from "../../images/icon-fog.webp";
-import IconStorm from "../../images/icon-storm.webp";
-import IconOvercast from "../../images/icon-overcast.webp";
-import IconSunny from "../../images/icon-sunny.webp";
-import IconSnow from "../../images/icon-snow.webp";
+import {
+  getDaysFromHourly,
+  getDayName,
+  getActiveFormattedHoursFromHourly,
+} from "../../utils/date";
+
+export type ForecastHourlyData = {
+  temperature_2m: Float32Array<ArrayBufferLike> | null;
+  weather_code: Float32Array<ArrayBufferLike> | null;
+  time: Date[];
+};
 
 export type HourlyForecast = {
+  data?: ForecastHourlyData;
   isLoading: boolean;
 };
 
-const HourlyForecast = ({ isLoading }: HourlyForecast) => {
-  const [days, setDays] = useState<DayDropdownValue[]>([
-    {
-      id: 1,
-      title: "Monday",
-      isActive: true,
-    },
-    {
-      id: 2,
-      title: "Tuesday",
-      isActive: false,
-    },
-    {
-      id: 3,
-      title: "Wednesday",
-      isActive: false,
-    },
-    {
-      id: 4,
-      title: "Thursday",
-      isActive: false,
-    },
-    {
-      id: 5,
-      title: "Friday",
-      isActive: false,
-    },
-    {
-      id: 6,
-      title: "Saturday",
-      isActive: false,
-    },
-    {
-      id: 7,
-      title: "Sunday",
-      isActive: false,
-    },
-  ]);
+const HourlyForecast = ({ data, isLoading }: HourlyForecast) => {
+  const [days, setDays] =
+    useState<{ id: number; title: Day; isActive: boolean }[]>();
+  const [hours, setHours] = useState<{ index: number; hour: Date }[]>();
+
+  useEffect(() => {
+    // get the correct days in the correct order from the data object
+    const numericDays = getDaysFromHourly(data?.time);
+
+    // create the days values in the form of DayDropdownValue type
+    const actualDays = numericDays.map((day, index) => {
+      return {
+        id: day,
+        title: getDayName(day) as Day,
+        isActive: index === 0 ? true : false,
+      };
+    });
+
+    setDays(actualDays);
+    setHours(getActiveFormattedHoursFromHourly(0, data));
+  }, [data]);
 
   const onDaysDropdownChangeHandler = (value: DayDropdownValue) => {
-    const newDays = days
+    const newDays = days!
       .map((day) => ({
         ...day,
         isActive: false,
       }))
       .map((day) => ({ ...day, isActive: day.id === value.id }));
 
+    setHours(getActiveFormattedHoursFromHourly(value.id, data));
     setDays(newDays);
   };
 
@@ -72,90 +61,26 @@ const HourlyForecast = ({ isLoading }: HourlyForecast) => {
         <DaysDropdown days={days} onChange={onDaysDropdownChangeHandler} />
       </div>
       <ul className="max-h-[37.125rem] overflow-y-scroll grid gap-[1rem] grid-cols-1">
-        <HourlyForecastItem
-          hour="3 PM"
-          temperature="20°"
-          image={IconRain}
-          isLoading={isLoading}
-        />
-        <HourlyForecastItem
-          hour="4 PM"
-          temperature="20°"
-          image={IconDrizzle}
-          isLoading={isLoading}
-        />
-        <HourlyForecastItem
-          hour="5 PM"
-          temperature="20°"
-          image={IconFog}
-          isLoading={isLoading}
-        />
-        <HourlyForecastItem
-          hour="6 PM"
-          temperature="20°"
-          image={IconStorm}
-          isLoading={isLoading}
-        />
-        <HourlyForecastItem
-          hour="7 PM"
-          temperature="20°"
-          image={IconOvercast}
-          isLoading={isLoading}
-        />
-        <HourlyForecastItem
-          hour="8 PM"
-          temperature="20°"
-          image={IconSunny}
-          isLoading={isLoading}
-        />
-        <HourlyForecastItem
-          hour="9 PM"
-          temperature="20°"
-          image={IconSnow}
-          isLoading={isLoading}
-        />
-        <HourlyForecastItem
-          hour="10 PM"
-          temperature="20°"
-          image={IconRain}
-          isLoading={isLoading}
-        />
-        <HourlyForecastItem
-          hour="11 PM"
-          temperature="20°"
-          image={IconRain}
-          isLoading={isLoading}
-        />
-        <HourlyForecastItem
-          hour="12 PM"
-          temperature="20°"
-          image={IconRain}
-          isLoading={isLoading}
-        />
-        <HourlyForecastItem
-          hour="1 AM"
-          temperature="20°"
-          image={IconRain}
-          isLoading={isLoading}
-        />
-        <HourlyForecastItem
-          hour="2 AM"
-          temperature="20°"
-          image={IconRain}
-          isLoading={isLoading}
-        />
-        <HourlyForecastItem
-          hour="3 AM"
-          temperature="20°"
-          image={IconRain}
-          isLoading={isLoading}
-        />
-        <HourlyForecastItem
-          hour="4 AM"
-          temperature="20°"
-          image={IconRain}
-          isLoading={isLoading}
-        />
+        {hours !== undefined &&
+          hours.map((hour) => {
+            const index = hour.index;
+            const actualHour = hour.hour.toLocaleString("en-US", {
+              hour: "numeric",
+              hour12: true,
+            });
+            const temperature = data?.temperature_2m![index];
+            const weatherCode = data?.weather_code![index];
+
+            return (
+              <HourlyForecastItem
+                key={index}
+                hour={actualHour}
+                temperature={Math.round(temperature!)}
+                weatherCode={weatherCode}
+                isLoading={isLoading}
+              />
+            );
+          })}
       </ul>
     </aside>
   );
--- src/components/HourlyForecastItem/HourlyForecastItem.tsx
+++ src/components/HourlyForecastItem/HourlyForecastItem.tsx
@@ -1,14 +1,16 @@
+import WeatherCode from "../WeatherCode/WeatherCode";
+
 export type HourlyForecastItem = {
-  hour: string;
-  temperature: string;
-  image: string;
+  hour?: string;
+  temperature?: number;
+  weatherCode?: number;
   isLoading: boolean;
 };
 
 const HourlyForecastItem = ({
   hour,
   temperature,
-  image,
+  weatherCode,
   isLoading,
 }: HourlyForecastItem) => {
   return (
@@ -16,10 +18,10 @@ const HourlyForecastItem = ({
       {!isLoading && (
         <li className="bg-background-3 rounded-[0.313rem] flex justify-between items-center h-[3.75rem] px-[1.125rem] border-[1px] border-light-blue dsktp:h-[3.75rem]">
           <div className="flex items-center gap-[1rem]">
-            <img src={image} alt="" className="w-[2.5rem]" />
+            <WeatherCode code={weatherCode} className="w-[2.5rem]" />
             <p>{hour}</p>
           </div>
-          <p>{temperature}</p>
+          <p>{temperature}°</p>
         </li>
       )}
       {isLoading && (
