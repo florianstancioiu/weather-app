d0d778dfd1a1dcef403d95c17abe78758fe84e40
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2025-09-16 09:46:19 +0300

    Handled fetch error with SomethingWrong comp

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import TodaysWeather from "./components/TodaysWeather/TodaysWeather";
 import DailyForecast from "./components/DailyForecast/DailyForecast";
 import HourlyForecast from "./components/HourlyForecast/HourlyForecast";
 import NoSearchResults from "./components/NoSearchResults/NoSearchResults";
+import SomethingWrong from "./components/SomethingWrong/SomethingWrong";
 
 import { type TodaysWeather as TodaysWeatherType } from "./components/TodaysWeather/TodaysWeather";
 import { type ForecastDailyData } from "./components/DailyForecast/DailyForecast";
@@ -19,6 +20,7 @@ type TodaysData = Omit<TodaysWeatherType, "isLoading">;
 const App = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [noSearchResults, setNoSearchResults] = useState(false);
+  const [isError, setIsError] = useState(false);
   const [isMetric, _setIsMetric] = useState(true);
   const [searchKeyword, setSearchKeyword] = useState("");
   const [todaysPrimaryData, setTodaysPrimaryData] = useState<
@@ -32,136 +34,141 @@ const App = () => {
 
   useEffect(() => {
     const getMeteoData = async () => {
-      if (searchKeyword.trim() === "") {
-        return;
-      }
+      try {
+        if (searchKeyword.trim() === "") {
+          return;
+        }
 
-      setIsLoading(true);
+        setIsLoading(true);
+        setIsError(false);
 
-      const geocodedCity = await fetchJson(
-        `https://geocoding-api.open-meteo.com/v1/search?name=${searchKeyword}&count=1&language=en&format=json`
-      );
+        const geocodedCity = await fetchJson(
+          `https://geocoding-api.open-meteo.com/v1/search?name=${searchKeyword}&count=1&language=en&format=json`
+        );
 
-      if (
-        geocodedCity?.results === undefined ||
-        geocodedCity?.results.length === 0
-      ) {
-        setNoSearchResults(true);
-        return;
-      }
+        if (
+          geocodedCity?.results === undefined ||
+          geocodedCity?.results.length === 0
+        ) {
+          setNoSearchResults(true);
+          return;
+        }
 
-      const { longitude, latitude, name, country } = geocodedCity?.results[0];
+        const { longitude, latitude, name, country } = geocodedCity?.results[0];
 
-      const params = {
-        latitude,
-        longitude,
-        daily: ["weather_code", "temperature_2m_max", "temperature_2m_min"],
-        hourly: ["temperature_2m", "weather_code"],
-        current: [
-          "temperature_2m",
-          "weather_code",
-          "precipitation",
-          "wind_speed_10m",
-          "relative_humidity_2m",
-          "apparent_temperature",
-        ],
-        timezone: "auto",
-      };
-      const url = "https://api.open-meteo.com/v1/forecast";
-      const responses = await fetchWeatherApi(url, params);
+        const params = {
+          latitude,
+          longitude,
+          daily: ["weather_code", "temperature_2m_max", "temperature_2m_min"],
+          hourly: ["temperature_2m", "weather_code"],
+          current: [
+            "temperature_2m",
+            "weather_code",
+            "precipitation",
+            "wind_speed_10m",
+            "relative_humidity_2m",
+            "apparent_temperature",
+          ],
+          timezone: "auto",
+        };
+        const url = "https://api.open-meteo.com/v1/forecast";
+        const responses = await fetchWeatherApi(url, params);
 
-      if (responses.length === 0) {
-        setNoSearchResults(true);
-        return;
-      }
+        if (responses.length === 0) {
+          setNoSearchResults(true);
+          return;
+        }
 
-      // Process first location. Add a for-loop for multiple locations or weather models
-      const response = responses[0];
+        // Process first location. Add a for-loop for multiple locations or weather models
+        const response = responses[0];
 
-      // Attributes for timezone and location
-      /*
-      const elevation = response.elevation();
-      const timezone = response.timezone();
-      const timezoneAbbreviation = response.timezoneAbbreviation();
-      */
-      const utcOffsetSeconds = response.utcOffsetSeconds();
+        // Attributes for timezone and location
+        /*
+        const elevation = response.elevation();
+        const timezone = response.timezone();
+        const timezoneAbbreviation = response.timezoneAbbreviation();
+        */
+        const utcOffsetSeconds = response.utcOffsetSeconds();
 
-      const current = response.current()!;
-      const hourly = response.hourly()!;
-      const daily = response.daily()!;
+        const current = response.current()!;
+        const hourly = response.hourly()!;
+        const daily = response.daily()!;
 
-      // Note: The order of weather variables in the URL query and the indices below need to match!
-      const weatherData = {
-        current: {
-          time: new Date((Number(current.time()) + utcOffsetSeconds) * 1000),
-          temperature_2m: current.variables(0)!.value(),
-          weather_code: current.variables(1)!.value(),
-          precipitation: current.variables(2)!.value(),
-          wind_speed_10m: current.variables(3)!.value(),
-          relative_humidity_2m: current.variables(4)!.value(),
-          apparent_temperature: current.variables(5)!.value(),
-        },
-        hourly: {
-          time: [
-            ...Array(
-              (Number(hourly.timeEnd()) - Number(hourly.time())) /
-                hourly.interval()
+        // Note: The order of weather variables in the URL query and the indices below need to match!
+        const weatherData = {
+          current: {
+            time: new Date((Number(current.time()) + utcOffsetSeconds) * 1000),
+            temperature_2m: current.variables(0)!.value(),
+            weather_code: current.variables(1)!.value(),
+            precipitation: current.variables(2)!.value(),
+            wind_speed_10m: current.variables(3)!.value(),
+            relative_humidity_2m: current.variables(4)!.value(),
+            apparent_temperature: current.variables(5)!.value(),
+          },
+          hourly: {
+            time: [
+              ...Array(
+                (Number(hourly.timeEnd()) - Number(hourly.time())) /
+                  hourly.interval()
+              ),
+            ].map(
+              (_, i) =>
+                new Date(
+                  (Number(hourly.time()) +
+                    i * hourly.interval() +
+                    utcOffsetSeconds) *
+                    1000
+                )
             ),
-          ].map(
-            (_, i) =>
-              new Date(
-                (Number(hourly.time()) +
-                  i * hourly.interval() +
-                  utcOffsetSeconds) *
-                  1000
-              )
-          ),
-          temperature_2m: hourly.variables(0)!.valuesArray(),
-          weather_code: hourly.variables(1)!.valuesArray(),
-        },
-        daily: {
-          time: [
-            ...Array(
-              (Number(daily.timeEnd()) - Number(daily.time())) /
-                daily.interval()
+            temperature_2m: hourly.variables(0)!.valuesArray(),
+            weather_code: hourly.variables(1)!.valuesArray(),
+          },
+          daily: {
+            time: [
+              ...Array(
+                (Number(daily.timeEnd()) - Number(daily.time())) /
+                  daily.interval()
+              ),
+            ].map(
+              (_, i) =>
+                new Date(
+                  (Number(daily.time()) +
+                    i * daily.interval() +
+                    utcOffsetSeconds) *
+                    1000
+                )
             ),
-          ].map(
-            (_, i) =>
-              new Date(
-                (Number(daily.time()) +
-                  i * daily.interval() +
-                  utcOffsetSeconds) *
-                  1000
-              )
-          ),
-          weather_code: daily.variables(0)!.valuesArray(),
-          temperature_2m_max: daily.variables(1)!.valuesArray(),
-          temperature_2m_min: daily.variables(2)!.valuesArray(),
-        },
-      };
+            weather_code: daily.variables(0)!.valuesArray(),
+            temperature_2m_max: daily.variables(1)!.valuesArray(),
+            temperature_2m_min: daily.variables(2)!.valuesArray(),
+          },
+        };
 
-      const currentTime = weatherData.current.time;
-      setTodaysPrimaryData({
-        city: name,
-        country: country,
-        date: `${getDayName(currentTime.getDay())}, ${getMonthShortName(
-          currentTime.getMonth()
-        )} ${currentTime.getDate()}, ${currentTime.getFullYear()}`,
-        temperature: weatherData.current.temperature_2m,
-        weatherCode: weatherData.current.weather_code,
-      });
+        const currentTime = weatherData.current.time;
+        setTodaysPrimaryData({
+          city: name,
+          country: country,
+          date: `${getDayName(currentTime.getDay())}, ${getMonthShortName(
+            currentTime.getMonth()
+          )} ${currentTime.getDate()}, ${currentTime.getFullYear()}`,
+          temperature: weatherData.current.temperature_2m,
+          weatherCode: weatherData.current.weather_code,
+        });
 
-      setTodaysSecondaryData({
-        precipitation: weatherData.current.precipitation,
-        wind: weatherData.current.wind_speed_10m,
-        humidity: current.variables(4)!.value(),
-        feelsLike: current.variables(5)!.value(),
-      });
+        setTodaysSecondaryData({
+          precipitation: weatherData.current.precipitation,
+          wind: weatherData.current.wind_speed_10m,
+          humidity: current.variables(4)!.value(),
+          feelsLike: current.variables(5)!.value(),
+        });
 
-      setDailyData(weatherData.daily);
-      setHourlyData(weatherData.hourly);
+        setDailyData(weatherData.daily);
+        setHourlyData(weatherData.hourly);
 
-      setIsLoading(false);
+        setIsLoading(false);
+      } catch {
+        setIsError(true);
+      }
     };
 
     getMeteoData();
@@ -175,30 +182,46 @@ const App = () => {
     setSearchKeyword(keyword);
   };
 
+  const onSomethingWrongRetryHandler = () => {
+    setIsLoading(false);
+    setNoSearchResults(false);
+    setIsError(false);
+    setSearchKeyword("");
+    setTodaysPrimaryData(undefined);
+    setTodaysSecondaryData(undefined);
+    setDailyData(undefined);
+    setHourlyData(undefined);
+  };
+
   return (
     <>
       <Header />
-      <SearchCity
-        onChange={onSearchCityChangeHandler}
-        onSearch={onSearchCitySearchHandler}
-      />
-      {noSearchResults && <NoSearchResults />}
-      {!noSearchResults && (
-        <div className="dsktp:w-[75.75rem] dsktp:mx-auto dsktp:grid dsktp:grid-cols-3 dsktp:items-start dsktp:gap-[2rem]">
-          <div className="dsktp:col-span-2">
-            <TodaysWeather
-              primaryData={todaysPrimaryData}
-              secondaryData={todaysSecondaryData}
-              isMetric={isMetric}
-              isLoading={isLoading}
-            />
-            <DailyForecast
-              data={dailyData}
-              isMetric={isMetric}
-              isLoading={isLoading}
-            />
-          </div>
-          <HourlyForecast data={hourlyData} isLoading={isLoading} />
+      {isError && <SomethingWrong onRetry={onSomethingWrongRetryHandler} />}
+      {!isError && (
+        <div>
+          <SearchCity
+            onChange={onSearchCityChangeHandler}
+            onSearch={onSearchCitySearchHandler}
+          />
+          {noSearchResults && <NoSearchResults />}
+          {!noSearchResults && (
+            <div className="dsktp:w-[75.75rem] dsktp:mx-auto dsktp:grid dsktp:grid-cols-3 dsktp:items-start dsktp:gap-[2rem]">
+              <div className="dsktp:col-span-2">
+                <TodaysWeather
+                  primaryData={todaysPrimaryData}
+                  secondaryData={todaysSecondaryData}
+                  isMetric={isMetric}
+                  isLoading={isLoading}
+                />
+                <DailyForecast
+                  data={dailyData}
+                  isMetric={isMetric}
+                  isLoading={isLoading}
+                />
+              </div>
+              <HourlyForecast data={hourlyData} isLoading={isLoading} />
+            </div>
+          )}
         </div>
       )}
     </>
--- src/components/SomethingWrong/SomethingWrong.tsx
+++ src/components/SomethingWrong/SomethingWrong.tsx
@@ -1,5 +1,30 @@
-const SomethingWrong = () => {
-  return <div></div>;
+import IconRetry from "../../images/icon-retry.svg?react";
+import IconError from "../../images/icon-error.svg?react";
+
+export type SomethingWrong = {
+  onRetry: () => void;
+};
+
+const SomethingWrong = ({ onRetry }: SomethingWrong) => {
+  return (
+    <div className="text-center mt-[4rem]">
+      <IconError className="w-[3.125rem] h-[2.625rem] mx-auto mb-[1.5rem]" />
+      <h1 className="font-bold leading-[120%] text-[3.25rem] mb-[1.5rem]">
+        Something went wrong
+      </h1>
+      <p className="font-medium text-[1.25rem] text-neutral leading-[120%] mb-[1.5rem] max-w-[34.625rem] mx-auto">
+        We couldn’t connect to the server (API error). Please try again in a few
+        moments.
+      </p>
+      <button
+        onClick={() => onRetry()}
+        className="bg-background-2 py-[0.75rem] px-[1.125rem] flex gap-[0.625rem] items-center cursor-pointer mx-auto rounded-[0.5rem]"
+      >
+        <IconRetry />
+        <span>Retry</span>
+      </button>
+    </div>
+  );
 };
 
 export default SomethingWrong;
