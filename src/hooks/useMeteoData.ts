51c07b91859b81941828f48e3c1a6f91500a01af
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2025-09-16 21:45:32 +0300

    Used the Geolocation API to load the weather
    for the current position
    Used an external API to reverse geocode the address, meaning to turn
    latitude and longitude in a human readable address

--- src/hooks/useMeteoData.ts
+++ src/hooks/useMeteoData.ts
@@ -20,8 +20,184 @@ const useMeteoData = () => {
   const [dailyData, setDailyData] = useState<ForecastDailyData>();
   const [hourlyData, setHourlyData] = useState<ForecastHourlyData>();
 
+  const resetState = () => {
+    setIsLoading(false);
+    setNoSearchResults(false);
+    setIsError(false);
+    setSearchKeyword("");
+    setTodaysPrimaryData(undefined);
+    setTodaysSecondaryData(undefined);
+    setDailyData(undefined);
+    setHourlyData(undefined);
+  };
+
+  const reverseGeocoding = async (latitude: number, longitude: number) => {
+    console.info("Retrieve the address from a given latitude and longitude");
+    try {
+      return await fetchJson(
+        `https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&apiKey=69b0b69ed6d341a8b1f49b3f6c89764a`,
+        {
+          method: "GET",
+        }
+      );
+    } catch {
+      setIsError(true);
+    }
+  };
+
+  const retriveDataFromOpenMeteo = async (
+    latitude: number,
+    longitude: number,
+    city?: string,
+    country?: string
+  ) => {
+    try {
+      let theCity = city;
+      let theCountry = country;
+
+      if (city === undefined || country === undefined) {
+        const reverseGeocodingData = await reverseGeocoding(
+          latitude,
+          longitude
+        );
+
+        const feature = reverseGeocodingData.features[0];
+        const properties = feature.properties;
+        theCity = properties.city as string;
+        theCountry = properties.country as string;
+      }
+
+      const params = {
+        latitude,
+        longitude,
+        daily: ["weather_code", "temperature_2m_max", "temperature_2m_min"],
+        hourly: ["temperature_2m", "weather_code"],
+        current: [
+          "temperature_2m",
+          "weather_code",
+          "precipitation",
+          "wind_speed_10m",
+          "relative_humidity_2m",
+          "apparent_temperature",
+        ],
+        timezone: "auto",
+      };
+      const url = "https://api.open-meteo.com/v1/forecast";
+      const responses = await fetchWeatherApi(url, params);
+
+      if (responses.length === 0) {
+        setNoSearchResults(true);
+        return;
+      }
+
+      // Process first location. Add a for-loop for multiple locations or weather models
+      const response = responses[0];
+      const utcOffsetSeconds = response.utcOffsetSeconds();
+
+      const current = response.current()!;
+      const hourly = response.hourly()!;
+      const daily = response.daily()!;
+
+      // Note: The order of weather variables in the URL query and the indices below need to match!
+      const weatherData = {
+        current: {
+          time: new Date((Number(current.time()) + utcOffsetSeconds) * 1000),
+          temperature_2m: current.variables(0)!.value(),
+          weather_code: current.variables(1)!.value(),
+          precipitation: current.variables(2)!.value(),
+          wind_speed_10m: current.variables(3)!.value(),
+          relative_humidity_2m: current.variables(4)!.value(),
+          apparent_temperature: current.variables(5)!.value(),
+        },
+        hourly: {
+          time: [
+            ...Array(
+              (Number(hourly.timeEnd()) - Number(hourly.time())) /
+                hourly.interval()
+            ),
+          ].map(
+            (_, i) =>
+              new Date(
+                (Number(hourly.time()) +
+                  i * hourly.interval() +
+                  utcOffsetSeconds) *
+                  1000
+              )
+          ),
+          temperature_2m: hourly.variables(0)!.valuesArray(),
+          weather_code: hourly.variables(1)!.valuesArray(),
+        },
+        daily: {
+          time: [
+            ...Array(
+              (Number(daily.timeEnd()) - Number(daily.time())) /
+                daily.interval()
+            ),
+          ].map(
+            (_, i) =>
+              new Date(
+                (Number(daily.time()) +
+                  i * daily.interval() +
+                  utcOffsetSeconds) *
+                  1000
+              )
+          ),
+          weather_code: daily.variables(0)!.valuesArray(),
+          temperature_2m_max: daily.variables(1)!.valuesArray(),
+          temperature_2m_min: daily.variables(2)!.valuesArray(),
+        },
+      };
+
+      const currentTime = weatherData.current.time;
+      setTodaysPrimaryData({
+        city: theCity !== undefined ? theCity : "",
+        country: theCountry !== undefined ? theCountry : "",
+        date: `${getDayName(currentTime.getDay())}, ${getMonthShortName(
+          currentTime.getMonth()
+        )} ${currentTime.getDate()}, ${currentTime.getFullYear()}`,
+        temperature: weatherData.current.temperature_2m,
+        weatherCode: weatherData.current.weather_code,
+      });
+
+      setTodaysSecondaryData({
+        precipitation: weatherData.current.precipitation,
+        wind: weatherData.current.wind_speed_10m,
+        humidity: current.variables(4)!.value(),
+        feelsLike: current.variables(5)!.value(),
+      });
+
+      setDailyData(weatherData.daily);
+      setHourlyData(weatherData.hourly);
+
+      setIsLoading(false);
+      setNoSearchResults(false);
+    } catch {
+      setIsError(true);
+    }
+  };
+
   useEffect(() => {
-    const getMeteoData = async () => {
+    const getMeteoDataOnPageLoad = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position: GeolocationPosition) => {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+
+            retriveDataFromOpenMeteo(latitude, longitude);
+          },
+          () => {
+            resetState();
+          }
+        );
+      }
+    };
+
+    getMeteoDataOnPageLoad();
+  }, []);
+
+  useEffect(() => {
+    const getMeteoDataOnKeywordChange = async () => {
       try {
         if (searchKeyword.trim() === "") {
           return;
@@ -42,125 +218,20 @@ const useMeteoData = () => {
           return;
         }
 
-        const { longitude, latitude, name, country } = geocodedCity?.results[0];
-
-        const params = {
-          latitude,
+        const {
           longitude,
-          daily: ["weather_code", "temperature_2m_max", "temperature_2m_min"],
-          hourly: ["temperature_2m", "weather_code"],
-          current: [
-            "temperature_2m",
-            "weather_code",
-            "precipitation",
-            "wind_speed_10m",
-            "relative_humidity_2m",
-            "apparent_temperature",
-          ],
-          timezone: "auto",
-        };
-        const url = "https://api.open-meteo.com/v1/forecast";
-        const responses = await fetchWeatherApi(url, params);
+          latitude,
+          name: city,
+          country,
+        } = geocodedCity?.results[0];
 
-        if (responses.length === 0) {
-          setNoSearchResults(true);
-          return;
-        }
-
-        // Process first location. Add a for-loop for multiple locations or weather models
-        const response = responses[0];
-
-        // Attributes for timezone and location
-        /*
-        const elevation = response.elevation();
-        const timezone = response.timezone();
-        const timezoneAbbreviation = response.timezoneAbbreviation();
-        */
-        const utcOffsetSeconds = response.utcOffsetSeconds();
-
-        const current = response.current()!;
-        const hourly = response.hourly()!;
-        const daily = response.daily()!;
-
-        // Note: The order of weather variables in the URL query and the indices below need to match!
-        const weatherData = {
-          current: {
-            time: new Date((Number(current.time()) + utcOffsetSeconds) * 1000),
-            temperature_2m: current.variables(0)!.value(),
-            weather_code: current.variables(1)!.value(),
-            precipitation: current.variables(2)!.value(),
-            wind_speed_10m: current.variables(3)!.value(),
-            relative_humidity_2m: current.variables(4)!.value(),
-            apparent_temperature: current.variables(5)!.value(),
-          },
-          hourly: {
-            time: [
-              ...Array(
-                (Number(hourly.timeEnd()) - Number(hourly.time())) /
-                  hourly.interval()
-              ),
-            ].map(
-              (_, i) =>
-                new Date(
-                  (Number(hourly.time()) +
-                    i * hourly.interval() +
-                    utcOffsetSeconds) *
-                    1000
-                )
-            ),
-            temperature_2m: hourly.variables(0)!.valuesArray(),
-            weather_code: hourly.variables(1)!.valuesArray(),
-          },
-          daily: {
-            time: [
-              ...Array(
-                (Number(daily.timeEnd()) - Number(daily.time())) /
-                  daily.interval()
-              ),
-            ].map(
-              (_, i) =>
-                new Date(
-                  (Number(daily.time()) +
-                    i * daily.interval() +
-                    utcOffsetSeconds) *
-                    1000
-                )
-            ),
-            weather_code: daily.variables(0)!.valuesArray(),
-            temperature_2m_max: daily.variables(1)!.valuesArray(),
-            temperature_2m_min: daily.variables(2)!.valuesArray(),
-          },
-        };
-
-        const currentTime = weatherData.current.time;
-        setTodaysPrimaryData({
-          city: name,
-          country: country,
-          date: `${getDayName(currentTime.getDay())}, ${getMonthShortName(
-            currentTime.getMonth()
-          )} ${currentTime.getDate()}, ${currentTime.getFullYear()}`,
-          temperature: weatherData.current.temperature_2m,
-          weatherCode: weatherData.current.weather_code,
-        });
-
-        setTodaysSecondaryData({
-          precipitation: weatherData.current.precipitation,
-          wind: weatherData.current.wind_speed_10m,
-          humidity: current.variables(4)!.value(),
-          feelsLike: current.variables(5)!.value(),
-        });
-
-        setDailyData(weatherData.daily);
-        setHourlyData(weatherData.hourly);
-
-        setIsLoading(false);
-        setNoSearchResults(false);
+        retriveDataFromOpenMeteo(latitude, longitude, city, country);
       } catch {
         setIsError(true);
       }
     };
 
-    getMeteoData();
+    getMeteoDataOnKeywordChange();
   }, [searchKeyword]);
 
   return {
@@ -180,6 +251,7 @@ const useMeteoData = () => {
     setTodaysSecondaryData,
     setDailyData,
     setHourlyData,
+    resetState,
   };
 };
 
