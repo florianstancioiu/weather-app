db7a9aa0a4b828e81c7cecd1d2c5bf444caf2fab
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2025-09-24 11:15:15 +0300

    Added tests for SomethingWrong component

---
+++ src/components/SomethingWrong/SomethingWrong.test.tsx
@@ -0,0 +1,33 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { vi } from "vitest";
+
+import SomethingWrong from "./SomethingWrong";
+
+describe("<SomethingWrong> component", () => {
+  test("renders the component", async () => {
+    const onRetryMock = vi.fn();
+
+    render(<SomethingWrong onRetry={onRetryMock} />);
+
+    const titleElement = screen.getByTestId("somethingWrong.title");
+    const buttonElement = screen.getByTestId("somethingWrong.button");
+
+    expect(titleElement).toBeInTheDocument();
+    expect(titleElement).toContainHTML("Something went wrong");
+    expect(buttonElement).toBeInTheDocument();
+    expect(onRetryMock).toHaveBeenCalledTimes(0);
+  });
+
+  test("triggeres the onRetry function prop on button click", async () => {
+    const onRetryMock = vi.fn();
+
+    render(<SomethingWrong onRetry={onRetryMock} />);
+
+    const buttonElement = screen.getByTestId("somethingWrong.button");
+
+    fireEvent.click(buttonElement);
+
+    expect(onRetryMock).toHaveBeenCalledTimes(1);
+    expect(onRetryMock).toHaveBeenCalledWith();
+  });
+});
